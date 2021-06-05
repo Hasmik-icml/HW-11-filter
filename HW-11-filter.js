744d44af54ea1b661ca091372a2f367940f9ac35
Hasmik_icml <gevorgyan.icml@gmail.com>
2021-06-05 12:06:31 +0400

    filter multiple of 5

---
+++ HW-11-filter.js
@@ -0,0 +1,14 @@
+//filter multiple of 5
+let arr = [4, 10, 8, 100, 15, 73, 9, 14];
+
+function filter(arr, func){
+  let newArr = [];
+  for(let i = 0; i < arr.length; ++i){
+     if(func(arr[i])){
+        newArr.push(arr[i]);
+     }
+  }
+  return newArr;
+}
+
+console.log(filter(arr, element => element % 5 === 0));
